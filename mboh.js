60681470b98dba3a5d89d66c58e90cd50f143dd1
goubanime <73523241+goubanime@users.noreply.github.com>
2020-10-29 14:55:01 +0700

    Update mboh.js

--- mboh.js
+++ mboh.js
@@ -20,3 +20,21 @@ $('.modal.stop-video-on-close').on('hidden.bs.modal', function(e) {
         this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
     });
 })
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
