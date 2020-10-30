e18b3c18ebbfb3e8b86b1b50ce227656e460bb03
goubanime <73523241+goubanime@users.noreply.github.com>
2020-10-31 01:32:11 +0700

    Update mboh.js

--- mboh.js
+++ mboh.js
@@ -15,11 +15,40 @@ $(document).ready(function() {
 });
 
 
-$('.modal.stop-video-on-close').on('hidden.bs.modal', function(e) {
-    $('.video-to-stop', this).each(function() {
-        this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
-    });
-})
+// var s1 = document.getElementById("s1");
+// var s2 = document.getElementById("s2");
+// var s3 = document.getElementById("s3");
+// var pilih1 = document.getElementById("pilih1");
+// var pilih2 = document.getElementById("pilih2");
+// var pilih3 = document.getElementById("pilih3");
+
+function season1() {
+   $('#s1').removeClass('d-none');
+   $('#s2').addClass('d-none');
+   $('#s3').addClass('d-none');
+   $('#pilih1').addClass('active');
+   $('#pilih2').removeClass('active');
+   $('#pilih3').removeClass('active');
+}
+
+function season2() {
+   $('#s1').addClass('d-none');
+   $('#s2').removeClass('d-none');
+   $('#s3').addClass('d-none');
+   $('#pilih1').removeClass('active');
+   $('#pilih2').addClass('active');
+   $('#pilih3').removeClass('active');
+}
+
+function season3() {
+   $('#s1').addClass('d-none');
+   $('#s2').addClass('d-none');
+   $('#s3').removeClass('d-none');
+   $('#pilih1').removeClass('active');
+   $('#pilih2').removeClass('active');
+   $('#pilih3').addClass('active');
+}
+
 
 document.onkeydown = function(e) {
   if(event.keyCode == 123) {
